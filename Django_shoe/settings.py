1f11cc36081c9f5fe427413d39ee72787070a5c2
GuestPopcorn <abidabdurahim1@gmail.com>
2022-12-17 00:39:17 +0500

    postgres

--- Django_shoe/settings.py
+++ Django_shoe/settings.py
@@ -88,8 +88,12 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'project_db',
+        'USER': 'sammy',
+        'PASSWORD': 'amoni@2009',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
